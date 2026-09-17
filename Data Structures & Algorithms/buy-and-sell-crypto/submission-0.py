70b91f1d5a37e9d074c1502edbf399ee4b3746d8
Anika Pai <pai57@purdue.edu>
2026-09-17 01:43:21 -0400

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -1,17 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        curr_profit = 0
-        min_price = float('inf')
-        max_profit = 0
+        ## Problem Restatement: You are given an array of integers representing the price of NeetCoin on the ith (index) day. You need to return the maxmium profit you can achieve by choosing a day to buy and a DIFFERENT day to sell
 
-        for price in prices:
-            if price < min_price:
-                min_price = price
+        ## Brute Force: Use two for-loops one iterates through the array once and the inner-for-loop runs every time the outer one iterates. It will keep calculating the profit everytime until it finds the greatest output. This would give us an O(n^2) time complexity.
 
-            curr_profit = price - min_price
+        ## More Optimized Solution: two pointers where one pointer only moves if the value of the item that the other pointer is pointing to is < than the first one. Keep calculating the profit and comparing it with max profit
 
-            if curr_profit > max_profit:
-                max_profit = curr_profit
-    
-        return max_profit
+        maxProf = 0
+        first = 0
+        sec = 1
+
+        while sec < len(prices):
+            maxProf = max(maxProf, prices[sec] - prices[first])
+            if prices[first] > prices[sec]:
+                first = sec
+            
+            sec += 1
+        
+        return maxProf
             
